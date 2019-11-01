62e2dea06891a78bbe1ded3ca7e6a1ffd37bf37a
Hunter5555 <hunter.m.allan@gmail.com>
2019-11-01 10:50:57 -0600

    tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,15 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+const tabs = document.querySelector(".topics");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    response.data.topics.forEach(singleTopic => {
+      tabs.appendChild(divTabs(singleTopic))
+    });
+    console.log(response);
+
+  });
+
